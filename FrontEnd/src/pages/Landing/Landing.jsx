f7c8600033fa6d259efe1e4983f5d09f57d14f0e
hyoryeon <jhl2_014@naver.com>
2026-01-15 17:53:05 +0900

    ts 시도하려다가 폭사

--- FrontEnd/src/pages/Landing/Landing.jsx
+++ FrontEnd/src/pages/Landing/Landing.jsx
@@ -1,8 +1,37 @@
+import './index.css'  
+
 export default function Landing(){
   return (
-    <div className="page landing">
+    <div className="landing">
       <h2>랜딩페이지</h2>
       <p>나 진짜 디자인 못하겠다</p>
+
+      <a href="#" className="neon-button">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Neon button
+      </a>
+
+            <a href="#" className="neon-button">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Neon button
+      </a>
+
+
+            <a href="#" className="neon-button">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Neon button
+      </a>
+
+
     </div>
   )
 }
